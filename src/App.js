8ca2e5b93284171f7e1102e3a19517ad64a07473
Samarth-4403 <samarth04042003@gmail.comm>
2023-08-02 15:58:03 +0530

    node modules reinstalled

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ml-auto">
